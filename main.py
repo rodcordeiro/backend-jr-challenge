2354a967afd7aa7f2f48b24e3b83dbf4d9b5ff92
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-02-20 07:23:21 -0300

    Edited main.py\nCreated request.txt

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import requests
-from datetime import datetime
 import json
+from datetime import datetime
 from decimal import Decimal
 
 
-payload={
- "nome": "Fulano da Silva",
- "cartao_credito": "5476045573597563",
- "data_compra": "2020-02-14",
- "valor_compra": 1000.00,
- "parcelamento": 4,
- "bandeira": "MASTER",
- "operacao": "CREDITO"
-}
+payload = json.load(open("request.txt",'r'))
+
 
 ##Funçoes para validar o payload
 def parcelamento(parcelamento):
@@ -20,8 +13,9 @@ def parcelamento(parcelamento):
         return True
     else:
         return False
+
 def parcelas(data_compra,valor_compra,desconto,parcelamento):
-    valor = round(((valor_compra-(valor_compra*desconto))/parcelamento),2)
+    valor_parcela = round(((valor_compra-(valor_compra*desconto))/parcelamento),2)
     parcelas = []
     date = datetime.strptime(payload['data_compra'],'%Y-%m-%d').date()
     mes = date.month
@@ -35,7 +29,7 @@ def parcelas(data_compra,valor_compra,desconto,parcelamento):
         parcela = parcela +1
         parcelas.append({
     		"data_pagamento": f"{ano}-{mes}-14",
-    		"valor": valor,
+    		"valor": valor_parcela,
     		"parcela": parcela
     	})
     return parcelas
@@ -52,38 +46,38 @@ def bandeira(bandeira):
 
 
 ##Função que irá validar o payload
-resposta={
+response={
  "transacao_success": "boolean",
  "cartao_credito_mascarado": "xxxx",
  "total_parcelas": 0,
  "pagamentos": [
  	{
-		"data_pagamento": "oooo-03-14",
-		"valor": 237.5,
-		"parcela": 1
+		"data_pagamento": "yyyy-mm-dd",
+		"valor": 000.00,
+		"parcela": 0
 	}
  ],
  "valor_compra": 000.00,
  "valor_pagamento": 000.00,
- "percentual_repasse": "x%"
+ "percentual_repasse": "0%"
 }
 
 def validar(payload):
-    resposta['cartao_credito_mascarado'] = payload["cartao_credito"][-4:]
-    resposta['total_parcelas']=payload['parcelamento']
+    response['cartao_credito_mascarado'] = payload["cartao_credito"][-4:]
+    response['total_parcelas']=payload['parcelamento']
     if bandeira(payload['bandeira']):
-        resposta["percentual_repasse"] = f"{(bandeira(payload['bandeira'])*100)}%"
+        response["percentual_repasse"] = f"{(bandeira(payload['bandeira'])*100)}%"
         if parcelamento(payload['parcelamento']):
-            resposta['pagamentos']=parcelas(payload["data_compra"],payload["valor_compra"],bandeira(payload['bandeira']),payload['parcelamento'])
-            resposta['valor_compra']=payload['valor_compra']
-            resposta['valor_pagamento']=payload['valor_compra']-(payload['valor_compra']*bandeira(payload['bandeira']))
+            response['pagamentos']=parcelas(payload["data_compra"],payload["valor_compra"],bandeira(payload['bandeira']),payload['parcelamento'])
+            response['valor_compra']=payload['valor_compra']
+            response['valor_pagamento']=payload['valor_compra']-(payload['valor_compra']*bandeira(payload['bandeira']))
         else:
             return False
     else:
         return False
 
-    resposta["transacao_success"] = True
-    return resposta
+    response["transacao_success"] = True
+    return response
 
 
 print(json.dumps(validar(payload)))
